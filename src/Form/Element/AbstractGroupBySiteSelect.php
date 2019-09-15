3c9fd60a1b034fc8b4290db249ac86c3398f9327
Daniel Berthereau <Daniel.github@Berthereau.net>
2019-09-16 00:00:00 +0200

    Added option for SitePageSelect to limit to selected sites.

--- src/Form/Element/AbstractGroupBySiteSelect.php
+++ src/Form/Element/AbstractGroupBySiteSelect.php
@@ -11,16 +11,41 @@ use Zend\Form\Element\Select;
 abstract class AbstractGroupBySiteSelect extends Select
 {
     /**
+     * @var SiteRepresentation
+     */
+    protected $site;
+
+    /**
      * @var ApiManager
      */
     protected $apiManager;
 
     /**
+     * @param SiteRepresentation $site
+     * @return self
+     */
+    public function setSite(SiteRepresentation $site = null)
+    {
+        $this->site = $site;
+        return $site;
+    }
+
+    /**
+     * @return \Omeka\Api\Representation\SiteRepresentation
+     */
+    public function getSite()
+    {
+        return $this->site;
+    }
+
+    /**
      * @param ApiManager $apiManager
+     * @return self
      */
     public function setApiManager(ApiManager $apiManager)
     {
         $this->apiManager = $apiManager;
+        return $this;
     }
 
     /**
@@ -46,6 +71,16 @@ abstract class AbstractGroupBySiteSelect extends Select
      */
     abstract public function getValueLabel($resource);
 
+    /**
+     * Specific options:
+     * - query;
+     * - site_group;
+     * - exclude_current_site;
+     * - disable_group_by_site.
+     *
+     * {@inheritDoc}
+     * @see \Zend\Form\Element\Select::getValueOptions()
+     */
     public function getValueOptions()
     {
         $query = $this->getOption('query');
@@ -53,14 +88,40 @@ abstract class AbstractGroupBySiteSelect extends Select
             $query = [];
         }
 
+        $currentSite = $this->getSite();
+        if ($currentSite) {
+            $currentSiteSlug = $currentSite->slug();
+            $excludeCurrentSite = $currentSite && $this->getOption('exclude_current_site');
+        } else {
+            $currentSiteSlug = null;
+            $excludeCurrentSite = false;
+        }
+
+        // Currently, the response cannot manage the option to exclude a site.
         $response = $this->getApiManager()->search($this->getResourceName(), $query);
 
-        if ($this->getOption('disable_group_by_site')) {
+        $siteGroup = $this->listSiteGroup();
+        $withinSiteGroup = $siteGroup !== false;
+        if ($excludeCurrentSite && $siteGroup) {
+            unset($siteGroup[$currentSiteSlug]);
+        }
+
+        $disableGroupBySite = (bool) $this->getOption('disable_group_by_site');
+        if ($disableGroupBySite) {
             // Group alphabetically by resource label without grouping by site.
             $resources = [];
             foreach ($response->getContent() as $resource) {
+                $site = $resource->site();
+                $siteSlug = $site ? $site->slug() : null;
+                if ($withinSiteGroup && !isset($siteGroup[$siteSlug])) {
+                    continue;
+                }
+                if ($excludeCurrentSite && $siteSlug === $currentSiteSlug) {
+                    continue;
+                }
                 $resources[$this->getValueLabel($resource)][] = $resource->id();
             }
+
             ksort($resources);
             $valueOptions = [];
             foreach ($resources as $label => $ids) {
@@ -69,15 +130,22 @@ abstract class AbstractGroupBySiteSelect extends Select
                 }
             }
         } else {
+            $currentSiteSlug = $currentSite->slug();
             // Group alphabetically by site title (but use slugs as keys).
             $resourceSites = [];
             $resourceSiteTitles = [];
             foreach ($response->getContent() as $resource) {
                 $site = $resource->site();
-                $index = $site ? $site->slug() : null;
-                $resourceSites[$index]['site'] = $site;
-                $resourceSites[$index]['resources'][] = $resource;
-                $resourceSiteTitles[$index] = $site->title();
+                $siteSlug = $site ? $site->slug() : null;
+                if ($withinSiteGroup && !isset($siteGroup[$siteSlug])) {
+                    continue;
+                }
+                if ($excludeCurrentSite && $siteSlug === $currentSiteSlug) {
+                    continue;
+                }
+                $resourceSites[$siteSlug]['site'] = $site;
+                $resourceSites[$siteSlug]['resources'][] = $resource;
+                $resourceSiteTitles[$siteSlug] = $site ? $site->title() : null;
             }
             natcasesort($resourceSiteTitles);
             $resourceSites = array_replace($resourceSiteTitles, $resourceSites);
@@ -108,5 +176,37 @@ abstract class AbstractGroupBySiteSelect extends Select
             $valueOptions = $prependValueOptions + $valueOptions;
         }
         return $valueOptions;
+    }
+
+    /**
+     * List the site groups of the current site.
+     *
+     * @todo Use something cleaner than a setting name in option?
+     *
+     * @return array|false Group of the current site.
+     */
+    protected function listSiteGroup()
+    {
+        $siteGroup = $this->getOption('site_group');
+        if (!$siteGroup) {
+            return false;
+        }
+
+        $site = $this->getSite();
+        if (empty($site)) {
+            return false;
+        }
+
+        $slug = $site->slug();
+
+        $settings = $site->getServiceLocator()->get('Omeka\Settings');
+        $siteGroups = $settings->get($siteGroup);
+        if (empty($siteGroups)) {
+            return [$slug => $slug];
+        }
+
+        return empty($siteGroups) || empty($siteGroups[$slug])
+            ? [$slug => $slug]
+            : array_combine($siteGroups[$slug], $siteGroups[$slug]);
     }
 }
--- src/Service/Form/Element/SitesPageSelectFactory.php
+++ src/Service/Form/Element/SitesPageSelectFactory.php
@@ -1,16 +1,19 @@
 <?php
 namespace Internationalisation\Service\Form\Element;
 
-use Interop\Container\ContainerInterface;
 use Internationalisation\Form\Element\SitesPageSelect;
+use Interop\Container\ContainerInterface;
 use Zend\ServiceManager\Factory\FactoryInterface;
 
 class SitesPageSelectFactory implements FactoryInterface
 {
     public function __invoke(ContainerInterface $services, $requestedName, array $options = null)
     {
+        $currentSite = $services->get('ControllerPluginManager')->get('currentSite');
+
         $element = new SitesPageSelect(null, $options);
         $element->setApiManager($services->get('Omeka\ApiManager'));
+        $element->setSite($currentSite());
         return $element;
     }
 }
